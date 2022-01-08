38efe8b9f846ddb8fda8134ef288a84c9fcca0be
UK-17 <utkarsh.kushagra@outlook.com>
2022-01-08 16:42:06 +0530

    bug fixes on frequency

--- core/medical_document_parser.py
+++ core/medical_document_parser.py
@@ -1,10 +1,9 @@
 import os
-import re #regex library
-import json #JSON library
-from pathlib import Path #to load the files
-import logging #logger
+import re 
+import json 
+from pathlib import Path
+import logging 
 import logging.config
-import textract
 from core.text_extractor import TextExtractor
 from core.medicine_scraper import MedicineScraper
 import sys
@@ -23,19 +22,20 @@ class MedicalExtractor:
 
     """ Extracts demographic/medicine data from a medical document. """
 
-    REGEX_FILE_PATH = str(FileOperations.get_project_root())+"/data/regex_mapping.json" #regex file path
-    FREQUENCY_MAPPING = str(FileOperations.get_project_root())+"/data/frequency_mapping.json" #frequency mapping file path
-    FIELDS_TO_EXTRACT = ['PATIENT_NAME','AGE','GENDER','MRN','DATE_OF_DISCHARGE','DOB','DIAGNOSIS'] #demographics field that we extract
-    DOCUMENT_SECTIONS = [('MEDICINE_SECTION',2),('DEMOGRAPHICS_SECTION',2),('EMERGENCY_SECTION',0)] #important sections of the document and threshold value for keywords to catch.
+    NO_OF_DAYS_TO_MONITOR = 7 # default monitoring duration
+    REGEX_FILE_PATH = str(FileOperations.get_project_root())+"/data/regex_mapping.json" # regex file path
+    FREQUENCY_MAPPING = str(FileOperations.get_project_root())+"/data/frequency_mapping.json" # frequency mapping file path
+    FIELDS_TO_EXTRACT = ['PATIENT_NAME','AGE','GENDER','MRN','DATE_OF_DISCHARGE','DOB','DIAGNOSIS'] # demographics field that we extract
+    DOCUMENT_SECTIONS = [('MEDICINE_SECTION',2),('DEMOGRAPHICS_SECTION',2),('EMERGENCY_SECTION',0)] # important sections of the document and threshold value for keywords to catch.
 
     def __init__(self,**kwargs) -> None:
 
         """ Initializing the Extractor by passing document. """
 
-        self.file_path = None #file path of the document passed
+        self.file_path = None # file path of the document passed
 
         try:
-            self.text = self.__extract_text(kwargs) #text extracted from file
+            self.text = self.__extract_text(kwargs) # text extracted from file
             logger.info(f'EXTRACTED TEXT : {self.text}')
         except Exception as e:
             msg = 'Error while extracting text from the document'
@@ -44,7 +44,7 @@ class MedicalExtractor:
             raise HTTPException(status_code=500, detail=msg)
         
         try:
-            self.regex_mapper = self.__load_regex() #load the regex from file
+            self.regex_mapper = self.__load_regex() # load the regex from file
         except Exception as e:
             msg = 'Regex mapping file is missing.'
             logger.error(e)
@@ -52,14 +52,14 @@ class MedicalExtractor:
             raise HTTPException(status_code=501,detail=msg)
 
         try:
-            self.sections = self.__break_in_sections() #divide the text into sections
+            self.sections = self.__break_in_sections() # divide the text into sections
         except Exception as e:
             msg = 'Could not break the text into sections.'
             logger.error(e)
             logger.critical(msg)
         
         try:
-            self.demographics_data = self.__extract_fields(MedicalExtractor.FIELDS_TO_EXTRACT) #extract demographics data
+            self.demographics_data = self.__extract_fields(MedicalExtractor.FIELDS_TO_EXTRACT) # extract demographics data
         except Exception as e:
             msg='Error while extracting demographics data.'
             logger.error(e)
@@ -67,7 +67,7 @@ class MedicalExtractor:
             self.demographics_data = None
 
         try:    
-            self.emergency_data = self.__extract_emergency_data() #extract emergency data
+            self.emergency_data = self.__extract_emergency_data() # extract emergency data
         except Exception as e:
             msg='Error while extracting emergency data.'
             logger.error(e)
@@ -75,7 +75,7 @@ class MedicalExtractor:
             self.emergency_data = None
         
         try:
-            self.medicines = self.__extract_medicines_regex() #extract medicines data
+            self.medicines = self.__extract_medicines_regex() # extract medicines data
         except Exception as e:
             msg='Error while extracting medicines.'
             logger.error(e)
@@ -83,7 +83,7 @@ class MedicalExtractor:
             self.medicines=None
         
         try:
-            self.output = self.__return_extracted_data() #final output 
+            self.output = self.__return_extracted_data() # final output 
         except Exception as e:
             msg='Error while generating output.'
             logger.error(e)
@@ -94,41 +94,41 @@ class MedicalExtractor:
 
         """ Loading the regex mapper file as a dictionary. """
 
-        regex_file = Path(MedicalExtractor.REGEX_FILE_PATH) #define the regex file
-        with open(regex_file, "r") as read_file: #open the regex file
-            mapper = json.load(read_file) #load the regex file as a dictionary
-        return mapper #return regex mapper
+        regex_file = Path(MedicalExtractor.REGEX_FILE_PATH) # define the regex file
+        with open(regex_file, "r") as read_file: # open the regex file
+            mapper = json.load(read_file) # load the regex file as a dictionary
+        return mapper # return regex mapper
 
     
     def __extract_text(self,params:dict) -> str:
 
         """ Extract data from the document and convert it to a string. """
 
-        file_path = params['input_file'] #filepath to the file
+        file_path = params['input_file'] # filepath to the file
         if '.docx' in file_path:method='DEFAULT'
         if '.pdf' in file_path:method='PDF_T'
         processed_text = TextExtractor(file_path=file_path,method=method)
         result = processed_text.extracted_text
-        return result #return extracted text
+        return result # return extracted text
     
     def __break_in_sections(self) -> list:
 
         """ Break the text into sections depending on the delimiter. """
 
-        delimiter = self.regex_mapper['DELIMITERS']['SECTION'] #delimiter on which the text will be broken into sections
-        whole_text = self.text #load text to temp variable
-        sections = re.split(delimiter,whole_text) #splitting the text on the delimiter
+        delimiter = self.regex_mapper['DELIMITERS']['SECTION'] # delimiter on which the text will be broken into sections
+        whole_text = self.text # load text to temp variable
+        sections = re.split(delimiter,whole_text) # splitting the text on the delimiter
         result = list()
         for para in sections:
             start = whole_text.find(para)
             end = start + len(para)
             tag = self.__tag_sections(para)
-            result.append({'tag':tag,'text':para,'span':(start,end)}) #indexing of the section
+            result.append({'tag':tag,'text':para,'span':(start,end)}) # indexing of the section
         
         
         
         logger.info(f'Tokenised Text : {result}')
-        return result #return a list of sections
+        return result # return a list of sections
     
     def __tag_sections(self,para:str) -> str:
 
@@ -150,8 +150,8 @@ class MedicalExtractor:
 
         """ Return a regex pattern for a given key from the regex mapper dictionary. """
 
-        search_dict = self.regex_mapper[keyword] #load the relevant regex dictionary
-        if nested: #if the pattern is nested
+        search_dict = self.regex_mapper[keyword] # load the relevant regex dictionary
+        if nested: # if the pattern is nested
             searchkey = search_dict['SEARCH_KEY']
             delimiter = search_dict['SEARCH_DELIMITER']
             pattern = search_dict['SEARCH_PATTERN']
@@ -161,7 +161,7 @@ class MedicalExtractor:
             raw_string = r"{}".format(pattern)
         if nested:
             logger.info(f'keyword:{keyword}|pattern:{raw_string}')
-        regex_str = re.compile(raw_string,re.IGNORECASE|re.DOTALL|re.MULTILINE) #add flags and make a regex pattern
+        regex_str = re.compile(raw_string,re.IGNORECASE|re.DOTALL|re.MULTILINE) # add flags and make a regex pattern
         return regex_str
     
     def __get_params(self,key_to_search,is_nested=True) -> str:
@@ -169,7 +169,7 @@ class MedicalExtractor:
         """ Extract the required field from the text. """
 
         try:
-            searchstr = self.__make_regex_pattern(nested=is_nested,keyword=key_to_search) #fetch the regex pattern
+            searchstr = self.__make_regex_pattern(nested=is_nested,keyword=key_to_search) # fetch the regex pattern
         except Exception as e:
             error = ErrorHandler(e,f"{key_to_search} not defined in regex_mapping.json")
             error.log_error()
@@ -179,14 +179,14 @@ class MedicalExtractor:
             retval = [each.groupdict() for each in re.finditer(searchstr,self.text)]
             logger.info('Regex caught {}:{}'.format(key_to_search,retval))
             retval = retval[0]
-            retval = retval[key_to_search] #caught data for the key
+            retval = retval[key_to_search] # caught data for the key
         except Exception as e:
             error = ErrorHandler(e,f'{key_to_search} not found')
             error.log_error()
             retval='N/A'
         
         logger.info("Param Returned:{} Value is :{}".format(key_to_search,retval))
-        return retval #returning value for the key
+        return retval # returning value for the key
     
 
     def __extract_fields(self,fields_to_extract:list) -> dict:
@@ -195,20 +195,20 @@ class MedicalExtractor:
 
         result = dict()
         for keyword in fields_to_extract:
-            extracted_information = self.__get_params(keyword) #extract data for a key
+            extracted_information = self.__get_params(keyword) # extract data for a key
             logger.info(f'{keyword}:{extracted_information}')
-            result[keyword]=extracted_information #add fielfname,caught data to collection
+            result[keyword]=extracted_information # add fielfname,caught data to collection
         
         logger.info(f'Extracted data : {result}')
-        return result #return all caught demographics data
+        return result # return all caught demographics data
     
     def __scrape_medicine(self,medicine:dict) -> dict:
 
         """ Getting brand name,generic name mapping for a medicine. """
 
         name = medicine['NAME']
-        med_scraper = MedicineScraper(name) #scraping for a search string
-        brand_name,generic_name,isExact = med_scraper.return_data() #getting metadata
+        med_scraper = MedicineScraper(name) # scraping for a search string
+        brand_name,generic_name,isExact = med_scraper.return_data() # getting metadata
         medicine['BRAND_NAME'] = brand_name
         medicine['GENERIC_NAME'] = generic_name
         del medicine['NAME']
@@ -220,38 +220,67 @@ class MedicalExtractor:
 
         result = list()
         for index,medicine in enumerate(medicines):
-                medicine['COMMENTS'] = self.medicines_section[medicine['span'][1]:medicines[index+1]['span'][0]-1].strip() if index!=len(medicines)-1 else '' #adding uncategorized information as comments
+                medicine['COMMENTS'] = self.medicines_section[medicine['span'][1]:medicines[index+1]['span'][0]-1].strip() if index!=len(medicines)-1 else '' # adding uncategorized information as comments
                 del medicine['span']
-                if medicine['DOSAGE'] is None and medicine['FREQUENCY'] is not None: #making frequency and dosage into understandable format
+                if medicine['DOSAGE'] is None and medicine['FREQUENCY'] is not None: # making frequency and dosage into understandable format
                     temp = medicine['FREQUENCY'].split('-')
                     medicine['FREQUENCY'] = '-'.join(['1' if each!='0' else '0' for each in temp])
                     medicine['DOSAGE']=temp[0]
                 
-                medicine = { k:('N/A' if v is None else v.strip()) for k, v in medicine.items()} #uncaught data is returned as 'N/A'
+                medicine = { k:('N/A' if v is None else v.strip()) for k, v in medicine.items()} # uncaught data is returned as 'N/A'
                 medicine = self.__scrape_medicine(medicine)
                 medicine = self.__correct_medicine_metadata(medicine)
                 result.append(medicine)
                     
-        return result #return cleaned up medicines
+        return result # return cleaned up medicines
     
-    def __correct_medicine_metadata(self,medicine:dict):
+    def __correct_medicine_frequency(self,medicine:dict):
 
-        """ Categorize comments in one of the fields. """
+        """ Try to interpret frequency of the medicine. """
 
         # correcting frequency term.
         comments = medicine['COMMENTS']
         frequency = medicine['FREQUENCY']
-        regex_file = Path(MedicalExtractor.FREQUENCY_MAPPING) #define the frequency file
-        with open(regex_file, "r") as read_file: #open the frequency file
-            frequency_mapper = json.load(read_file) #load the frequency file as a dictionary
+        regex_file = Path(MedicalExtractor.FREQUENCY_MAPPING) # define the frequency file
+        with open(regex_file, "r") as read_file: # open the frequency file
+            frequency_mapper = json.load(read_file) # load the frequency file as a dictionary
             if frequency == 'N/A' and len(comments)>0:
                 for term,synonyms in frequency_mapper.items():
                     for each in synonyms:
                         if each.lower() in comments.lower():
                             frequency = term       
-        medicine['FREQUENCY'] = frequency
+                medicine['FREQUENCY'] = frequency
 
         return medicine
+
+    def __correct_medicine_duration(self,medicine:dict):
+
+        """ Try to interpret frequency of the medicine. """
+
+        # correcting duration term.
+        comments = medicine['COMMENTS']
+        duration = medicine['DURATION']
+        if duration == 'N/A' and len(comments)>0:
+            if 'continue' in comments.lower():
+                duration = MedicalExtractor.NO_OF_DAYS_TO_MONITOR
+                medicine['DURATION'] = duration
+
+        return medicine
+
+    
+    def __correct_medicine_metadata(self,medicine:dict):
+
+        """ Categorize comments in one of the fields. """
+
+        logger.info('Correcting medicine metadata.')
+
+        medicine = self.__correct_medicine_frequency(medicine) # correcting frequency for the medicine
+        medicine = self.__correct_medicine_duration(medicine) # correcting duration for the medicine
+
+        return medicine
+
+
+        
 
     
     def __extract_medicines_regex(self) -> list:
@@ -259,25 +288,25 @@ class MedicalExtractor:
         """ Extract Medicines by regex method. """
 
         medicines = list()
-        medicine_pattern = self.__make_regex_pattern(keyword='MEDICINE',nested=False) #medicine regex pattern
+        medicine_pattern = self.__make_regex_pattern(keyword='MEDICINE',nested=False) # medicine regex pattern
         try:
             self.medicines_section = [each['text'] for each in self.sections if each['tag']=='MEDICINE_SECTION'][0]
         except Exception as e:
             error = ErrorHandler(e,'No Medical section found. Searching for medicines in the entire document.')
             error.log_error()
             self.medicines_section = self.text
-        retval = re.finditer(medicine_pattern,self.medicines_section) #match objects for all caught medicines
-        for match in retval: #structure medicine into list of dictionaries
+        retval = re.finditer(medicine_pattern,self.medicines_section) # match objects for all caught medicines
+        for match in retval: # structure medicine into list of dictionaries
             logger.info(match)
             span = match.span()
             medicine_data = match.groupdict()
             medicine_data['span']=span
             medicines.append(medicine_data)
         
-        medicines = self.__refine_medicines(medicines) #cleanup up medicines
+        medicines = self.__refine_medicines(medicines) # cleanup up medicines
             
         logger.info(medicines)
-        return medicines #returning medicines caught by regex
+        return medicines # returning medicines caught by regex
     
     def __extract_emergency_data(self) -> str:
 
